ef0459f84e0c69923ddc55184f9fbf408227f17d
Rajiv Singh <rajivpaulsingh@gmail.com>
2020-05-20 16:34:11 -0700

    Created the first test

---
+++ cypress/integration/first_test.spec.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+/* eslint-disable no-undef */
+
+describe('Heading text', () => {
+    it('contains the correct title', () => {
+        cy.visit('http://localhost:3000/example-1');
+        cy.get('h1')
+            .invoke('text')
+            .should('equal', 'My Awesome Web Application')
+    });
+});

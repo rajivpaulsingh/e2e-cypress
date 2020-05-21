2b73fffb40311ad67da8cc2c9829e4a7cb5decc6
Rajiv Singh <rajivpaulsingh@gmail.com>
2020-05-21 16:07:21 -0700

    Added test for double click

---
+++ cypress/integration/interactions.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress" />
+/* eslint-disable no-undef */
+
+describe('Basic page interations', () => {
+
+    beforeEach(() => {
+        cy.visit('example-4');
+    });
+
+    it('sets the header text to the item\'s name when double clicked', () => {
+        cy.get('[data-cy=box-1-items-list] > :nth-child(2)')
+            .dblclick();
+
+        cy.get('[data-cy=box-1-selected-name]')
+            .invoke('text')
+            .should('equal', 'Option Two');    
+    });
+});
